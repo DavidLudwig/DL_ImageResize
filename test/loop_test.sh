fe5709767ae7f99eed5d806986d1fa06c4b12272
David Ludwig <dludwig@pobox.com>
2017-10-04 23:14:23 -0400

    loop-testing miscellany

--- test/loop_test.sh
+++ test/loop_test.sh
@@ -4,11 +4,14 @@
 # TOTAL=2500
 # SIZE=1024x768
 
+MEASURE=100
+TOTAL=500
+
 # MEASURE=250
 # TOTAL=1250
 
-MEASURE=500
-TOTAL=2500
+# MEASURE=500
+# TOTAL=2500
 
 # MEASURE=2000
 # TOTAL=10000
@@ -18,7 +21,7 @@ SIZE=1920x1080
 while true; do
 	echo "----------------------------------------"
 	echo "new"
-	./test -f /Users/davidl/Desktop/test_full.bmp -h -s $SIZE -n $MEASURE -t $TOTAL -r bi11
+	./test -f /Users/davidl/Desktop/test_full.bmp -h -s $SIZE -n $MEASURE -t $TOTAL -r DLIR
 	echo "benchmark"
-	./test -f /Users/davidl/Desktop/test_full.bmp -h -s $SIZE -n $MEASURE -t $TOTAL -r bi10
+	./test -f /Users/davidl/Desktop/test_full.bmp -h -s $SIZE -n $MEASURE -t $TOTAL -r Bi11
 done
