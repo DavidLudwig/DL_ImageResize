a3d87acd20fce4cc0eaf3dd19538e4dfdd950ca2
David Ludwig <dludwig@pobox.com>
2017-10-13 21:43:31 -0400

    dual-licensed DL_ImageResize under either CC0 (public domain) or zlib (an OSI-compliant license)

--- DL_ImageResize.h
+++ DL_ImageResize.h
@@ -1,5 +1,13 @@
-// DL_ImageResize.h
-// zlib-licensed, Oct 8 2017, David Ludwig, dludwig@pobox.com. See "COPYING" statement at the end of this file.
+//
+// DL_ImageResize -- an image resizing library for C/C++
+// Written by David Lee Ludwig <dludwig@pobox.com>
+//
+// This project is dual-licensed under the CC0 (public domain) and zlib licenses.
+//
+// You may use this code under the terms of either license.
+//
+// See "COPYING" statement at the end of this file.
+//
 
 #ifndef DL_ImageResize_h
 #define DL_ImageResize_h
@@ -537,20 +545,41 @@ DLIR_EXTERN_C void DLIR_ResizeBilinear_ARGB8888(
 #endif // defined(DLIR_IMPLEMENTATION)
 
 
-
 //
 // COPYING
 //
+// This project is dual-licensed under the CC0 (public domain) and zlib licenses.
+// 
+// You may use this code under the terms of either license.
+// 
+// ---------------------------------------------------------------------------
+// CC0 License:
+// 
+// DL_ImageResize -- an image resizing library for C/C++
+// 
+// Written in 2017 by David Lee Ludwig <dludwig@pobox.com>
+// 
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain
+// worldwide. This software is distributed without any warranty.
+// 
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+// 
+// ---------------------------------------------------------------------------
+// zlib License:
+// 
+// DL_ImageResize -- an image resizing library for C/C++
 // Copyright (c) 2017 David Lee Ludwig
-//
+// 
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
 // arising from the use of this software.
-//
+// 
 // Permission is granted to anyone to use this software for any purpose,
 // including commercial applications, and to alter it and redistribute it
 // freely, subject to the following restrictions:
-//
+// 
 // 1. The origin of this software must not be misrepresented; you must not
 //    claim that you wrote the original software. If you use this software
 //    in a product, an acknowledgment in the product documentation would be
@@ -558,6 +587,8 @@ DLIR_EXTERN_C void DLIR_ResizeBilinear_ARGB8888(
 // 2. Altered source versions must be plainly marked as such, and must not be
 //    misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
-//
+// 
 //     David Lee Ludwig <dludwig@pobox.com>
-//
+// 
+// ---------------------------------------------------------------------------
+// 
--- test/x86_3.nasm
+++ test/x86_3.nasm
@@ -1,5 +1,13 @@
-; x86_3.nasm
-; zlib-licensed, Oct 8 2017, David Ludwig, dludwig@pobox.com. See "COPYING" statement at the end of this file.
+;
+; DL_ImageResize -- an image resizing library for C/C++
+; Written by David Lee Ludwig <dludwig@pobox.com>
+;
+; This project is dual-licensed under the CC0 (public domain) and zlib licenses.
+;
+; You may use this code under the terms of either license.
+;
+; See "COPYING" statement at the end of this file.
+;
 
 global _BilinearScale3_x86
 
@@ -337,16 +345,38 @@ _BilinearScale3_x86:
 ;
 ; COPYING
 ;
+; This project is dual-licensed under the CC0 (public domain) and zlib licenses.
+; 
+; You may use this code under the terms of either license.
+; 
+; ---------------------------------------------------------------------------
+; CC0 License:
+; 
+; DL_ImageResize -- an image resizing library for C/C++
+; 
+; Written in 2017 by David Lee Ludwig <dludwig@pobox.com>
+; 
+; To the extent possible under law, the author(s) have dedicated all copyright
+; and related and neighboring rights to this software to the public domain
+; worldwide. This software is distributed without any warranty.
+; 
+; You should have received a copy of the CC0 Public Domain Dedication along with
+; this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+; 
+; ---------------------------------------------------------------------------
+; zlib License:
+; 
+; DL_ImageResize -- an image resizing library for C/C++
 ; Copyright (c) 2017 David Lee Ludwig
-;
+; 
 ; This software is provided 'as-is', without any express or implied
 ; warranty. In no event will the authors be held liable for any damages
 ; arising from the use of this software.
-;
+; 
 ; Permission is granted to anyone to use this software for any purpose,
 ; including commercial applications, and to alter it and redistribute it
 ; freely, subject to the following restrictions:
-;
+; 
 ; 1. The origin of this software must not be misrepresented; you must not
 ;    claim that you wrote the original software. If you use this software
 ;    in a product, an acknowledgment in the product documentation would be
@@ -354,6 +384,8 @@ _BilinearScale3_x86:
 ; 2. Altered source versions must be plainly marked as such, and must not be
 ;    misrepresented as being the original software.
 ; 3. This notice may not be removed or altered from any source distribution.
-;
+; 
 ;     David Lee Ludwig <dludwig@pobox.com>
-;
+; 
+; ---------------------------------------------------------------------------
+; 
